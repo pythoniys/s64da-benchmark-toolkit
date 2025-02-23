177e9440ba6c79b941cb4436a543722e8802d444
pythoniys <167799216+pythoniys@users.noreply.github.com>
2025-02-23 16:40:26 +0300

    Update stats.py

--- benchmarks/htap/lib/stats.py
+++ benchmarks/htap/lib/stats.py
@@ -288,36 +288,35 @@ class Stats:
 
     # TODO rework this summary, the output summary is kind of useless, probably should be more inline with the stdout display
     def write_summary(self, csv_file, elapsed):
-def write_summary(self, csv_file, elapsed):
-    with open(csv_file, 'w') as csv:
-        # Проверяем, открыт ли файл и можно ли его читать
-        if self.csv_olap and not self.csv_olap.closed:
-            self.csv_olap.close()
-        if self.csv_olap:
-            with open(self.csv_olap.name, 'r') as f:
-                csv.write(f.read())
-                csv.write('\n')
-
-        if self.csv_olap_stream and not self.csv_olap_stream.closed:
-            self.csv_olap_stream.close()
-        if self.csv_olap_stream:
-            with open(self.csv_olap_stream.name, 'r') as f:
-                csv.write(f.read())
-                csv.write('\n')
-
-        if self.csv_oltp and not self.csv_oltp.closed:
-            self.csv_oltp.close()
-        if self.csv_oltp:
-            with open(self.csv_oltp.name, 'r') as f:
-                csv.write(f.read())
-                csv.write('\n')
-
-        if self.csv_dbstats and not self.csv_dbstats.closed:
-            self.csv_dbstats.close()
-        if self.csv_dbstats:
-            with open(self.csv_dbstats.name, 'r') as f:
-                csv.write(f.read())
-                csv.write('\n')
+        with open(csv_file, 'w') as csv:
+            # Проверяем, открыт ли файл и можно ли его читать
+            if self.csv_olap and not self.csv_olap.closed:
+                self.csv_olap.close()
+            if self.csv_olap:
+                with open(self.csv_olap.name, 'r') as f:
+                    csv.write(f.read())
+                    csv.write('\n')
+    
+            if self.csv_olap_stream and not self.csv_olap_stream.closed:
+                self.csv_olap_stream.close()
+            if self.csv_olap_stream:
+                with open(self.csv_olap_stream.name, 'r') as f:
+                    csv.write(f.read())
+                    csv.write('\n')
+    
+            if self.csv_oltp and not self.csv_oltp.closed:
+                self.csv_oltp.close()
+            if self.csv_oltp:
+                with open(self.csv_oltp.name, 'r') as f:
+                    csv.write(f.read())
+                    csv.write('\n')
+    
+            if self.csv_dbstats and not self.csv_dbstats.closed:
+                self.csv_dbstats.close()
+            if self.csv_dbstats:
+                with open(self.csv_dbstats.name, 'r') as f:
+                    csv.write(f.read())
+                    csv.write('\n')
 
         # row_nr = 0
         # with open(csv_file, 'w') as csv:
